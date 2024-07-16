94364f5648666a8de2d8e611d6356878bf6ae42d
supipis <nirmanialgama@gmail.com>
2024-07-16 20:47:38 +0200

    initial commit

---
+++ src/main/java/com/greenconnect/green_connect_api/GreenConnectApiApplication.java
@@ -0,0 +1,13 @@
+package com.greenconnect.green_connect_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenConnectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenConnectApiApplication.class, args);
+	}
+
+}
