3a77b414aaa7f3439bd18b99078f7178a47c7956
supipis <nirmanialgama@gmail.com>
2024-07-16 22:32:17 +0200

    implement exception handler

---
+++ src/main/java/com/greenconnect/green_connect_api/service/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.greenconnect.green_connect_api.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
